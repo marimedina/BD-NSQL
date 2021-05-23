23c2500ce3112bc15433f61117ff3253fdfef67a
marimedina <mariescuela@outlook.com>
2021-05-22 21:05:34 -0300

    ..

--- Flask-Redis/api/app.py
+++ Flask-Redis/api/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 import redis
+from flask import render_template, request
+
 
 app = Flask(__name__)
 
@@ -12,15 +14,32 @@ def connect_db():
     return conexion
 
 
-@app.route('/')
+def get_lista(nombre):
+    db = connect_db()
+    db.lpush(nombre, '1: The Mandalorian, 2: The Child, 3: The Sin, 4: Sanctuary, 5: The Gunslinger, 6: The Prisoner, 7: The Reckoning, 8: Redemption')
+    lista = db.lrange(nombre,0,-1)
+    return lista
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    connect_db()
-    return 'Hola Mundo'
+    return render_template('/index.html', msj='Hola desde home')
 
 
 @app.route('/about')
 def about():
-    return 'About Python Flask'
+    return render_template('/about.html', msj='Hola desde about')
+
+
+
+@app.route('/capitulos')
+def capitulos():
+    con = connect_db()
+    lista = get_lista('capitulos')
+    return render_template('/capitulos.html', datos=lista)
+
+
+
 
 if __name__ == '__main__':
-    app.run(host='localhost',port='5000', debug=False)
+    app.run(host='web-app-flask', port='5000', debug=True)
